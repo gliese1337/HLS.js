0ce8fbde40cb10788df507f4f7ceabfd58e777b3
Logan Kearsley <chronosurfer@gmail.com>
2015-05-05 18:34:41 -0600

    Refactor & simplify demo.

--- browser.js
+++ browser.js
@@ -2,30 +2,43 @@
 	'use strict';
 
 	var worker = new Worker('worker.js'),
-		nextIndex = 0,
-		sentVideos = 0,
-		currentVideo = null,
+		videoIndex = 0,
 		videos = [],
-		lastOriginal,
 		canvas = document.getElementById('canvas'),
 		context = canvas.getContext('2d'),
-		startbtn = document.getElementById('start'),
+		playbtn = document.getElementById('play'),
 		manifest = document.getElementById('manifest'),
 		decryptor = new AESDecryptor();
 
 	// drawing new frame
-	function nextFrame(){
+	function nextFrame(currentVideo){
 		if(currentVideo.paused || currentVideo.ended){
 			return;
 		}
 		context.drawImage(currentVideo, 0, 0);
-		requestAnimationFrame(nextFrame);
+		requestAnimationFrame(function(){ nextFrame(currentVideo); });
 	}
 
-	startbtn.addEventListener('click', function(){
+	function playpause(){
+		var currentVideo = videos[videoIndex];
+		if(!currentVideo){ return; }
+		if(this.textContent === "Pause"){
+			this.textContent = "Play";
+			currentVideo.pause();
+		}else{
+			this.textContent = "Pause";
+			currentVideo.play();
+		}
+	}
+
+	function init(){
 		getStream(manifest.value);
-		this.disabled = true;
-	}, false);
+		playbtn.removeEventListener('click', init, false);
+		playbtn.textContent = "Pause";
+		playbtn.addEventListener('click', playpause, false);
+	}
+
+	playbtn.addEventListener('click', init, false);
 
 	worker.addEventListener('message', function(event){
 		var video, data = event.data,
@@ -34,7 +47,7 @@
 		switch(data.type){
 			// worker is ready to convert
 			case 'ready':
-				startbtn.disabled = false;
+				playbtn.disabled = false;
 				return;
 
 			// got new converted MP4 video data
@@ -49,48 +62,25 @@
 				});
 
 				video.addEventListener('play', function(){
-					if(currentVideo !== this){
-						if(!currentVideo){
-							document.body.classList.remove('loading');
-							['play', 'pause'].forEach(function(action){
-								document.getElementById(action).addEventListener('click', function(){
-									document.body.classList.toggle('paused');
-									currentVideo[action]();
-								});
-							});
-						}
-						console.log('playing ' + descriptor);
-						currentVideo = this;
-						nextIndex++;
-						/*if(sentVideos - nextIndex <= 1){
-							getMore();
-						}*/
-					}
-					nextFrame();
-				});
+					console.log("playing", descriptor);
+					nextFrame(this);
+				}, false);
 
 				video.addEventListener('ended', function(){
-					delete videos[nextIndex - 1];
-					if(nextIndex in videos){
-						videos[nextIndex].play();
+					videoIndex++;
+					if(videoIndex < videos.length){
+						videos[videoIndex].play();
 					}
 				});
-				if(video.src.slice(0, 5) === 'blob:'){
-					video.addEventListener('ended', function(){
-						URL.revokeObjectURL(this.src);
-					});
-				}
 
-				video.src = data.url;
+				video.src = URL.createObjectURL(new Blob([data.bytes], {type: 'video/mp4'}));
 				video.load();
 
 				console.log('converted ' + descriptor);
 				videos[data.index] = video;
-				if((!currentVideo || currentVideo.ended) && data.index === nextIndex){
+				if(videoIndex === videos.length - 1){
 					video.play();
 				}
-
-				return;
 		}
 	});
 
@@ -113,7 +103,7 @@
 	})();
 
 	// loading more videos from manifest
-	function loadSegments(manifest, segments, sent, i){
+	function loadSegments(manifest, segments, i){
 		if(i === segments.length || i === 20){ return; }
 		var seg = segments[i],
 			url = resolveURL(manifest, seg.uri);
@@ -126,7 +116,7 @@
 			xhr.open("GET", url, true);
 			xhr.send();
 		})).then(function(arrbuffer){
-			loadSegments(manifest, segments, sent, i+1);
+			loadSegments(manifest, segments, i+1);
 
 			var data = new Uint8Array(arrbuffer);
 			if(seg.encryption.method === "AES-128"){
@@ -137,7 +127,7 @@
 			}
 			return data;
 		}).then(function(data){
-			worker.postMessage({buffer: data, url: url, index: sent + i}, [data.buffer]);
+			worker.postMessage({buffer: data, url: url, index: i}, [data.buffer]);
 		});
 	}
 	
@@ -145,8 +135,7 @@
 		var ajax = new XMLHttpRequest();
 		ajax.addEventListener('load', function(){
 			var segments = parseHLS(this.responseText).slice(0,20);
-			loadSegments(url, segments, sentVideos, 0);
-			sentVideos += segments.length;
+			loadSegments(url, segments, 0);
 
 			console.log('asked for ' + segments.length + ' more videos');
 		});
---
+++ mp4_build.js
@@ -0,0 +1,330 @@
+function MP4_build(vdata, adata, jBinary, MP4){
+	var mp4, trak,
+		creationTime = new Date(),
+		audioStart = vdata.data.byteLength,
+		mdat = new Uint8Array(vdata.data.byteLength + adata.data.byteLength);
+
+	mdat.set(vdata.data);
+	mdat.set(adata.data,audioStart);
+
+	// generating resulting MP4
+
+	mp4 = new jBinary(mdat.byteLength*2, MP4);
+	
+	trak = [{
+		atoms: {
+			tkhd: [{
+				version: 0,
+				flags: 15,
+				track_ID: 1,
+				duration: vdata.duration,
+				layer: 0,
+				alternate_group: 0,
+				volume: 1,
+				matrix: {
+					a: 1, b: 0, x: 0,
+					c: 0, d: 1, y: 0,
+					u: 0, v: 0, w: 1
+				},
+				dimensions: {
+					horz: vdata.width,
+					vert: vdata.height
+				}
+			}],
+			mdia: [{
+				atoms: {
+					mdhd: [{
+						version: 0,
+						flags: 0,
+						timescale: 90000,
+						duration: vdata.duration,
+						lang: 'und'
+					}],
+					hdlr: [{
+						version: 0,
+						flags: 0,
+						handler_type: 'vide',
+						name: 'VideoHandler'
+					}],
+					minf: [{
+						atoms: {
+							vmhd: [{
+								version: 0,
+								flags: 1,
+								graphicsmode: 0,
+								opcolor: {r: 0, g: 0, b: 0}
+							}],
+							dinf: [{
+								atoms: {
+									dref: [{
+										version: 0,
+										flags: 0,
+										entries: [{
+											type: 'url ',
+											version: 0,
+											flags: 1,
+											location: ''
+										}]
+									}]
+								}
+							}],
+							stbl: [{
+								atoms: {
+									stsd: [{
+										version: 0,
+										flags: 0,
+										entries: [{
+											type: 'avc1',
+											data_reference_index: 1,
+											dimensions: {
+												horz: vdata.width,
+												vert: vdata.height
+											},
+											resolution: {
+												horz: 72,
+												vert: 72
+											},
+											frame_count: 1,
+											compressorname: '',
+											depth: 24,
+											atoms: {
+												avcC: [{
+													version: 1,
+													profileIndication: vdata.spsInfo.profile_idc,
+													profileCompatibility: vdata.spsInfo.profile_compatibility,
+													levelIndication: vdata.spsInfo.level_idc,
+													lengthSizeMinusOne: 3,
+													seqParamSets: [vdata.sps],
+													pictParamSets: [vdata.pps]
+												}]
+											}
+										}]
+									}],
+									stts: [{
+										version: 0,
+										flags: 0,
+										entries: vdata.dts_diffs
+									}],
+									stss: [{
+										version: 0,
+										flags: 0,
+										entries: vdata.accessIndices
+									}],
+									ctts: [{
+										version: 0,
+										flags: 0,
+										entries: vdata.pd_diffs
+									}],
+									stsc: [{
+										version: 0,
+										flags: 0,
+										entries: [{
+											first_chunk: 1,
+											samples_per_chunk: vdata.sizes.length,
+											sample_description_index: 1
+										}]
+									}],
+									stsz: [{
+										version: 0,
+										flags: 0,
+										sample_size: 0,
+										sample_count: vdata.sizes.length,
+										sample_sizes: vdata.sizes
+									}],
+									stco: [{
+										version: 0,
+										flags: 0,
+										entries: [0x28]
+									}]
+								}
+							}]
+						}
+					}]
+				}
+			}]
+		}
+	}];
+
+	if(adata.audioSize > 0){
+		trak.push({
+			atoms: {
+				tkhd: [{
+					version: 0,
+					flags: 15,
+					track_ID: 2,
+					duration: vdata.duration,
+					layer: 0,
+					alternate_group: 1,
+					volume: 1,
+					matrix: {
+						a: 1, b: 0, x: 0,
+						c: 0, d: 1, y: 0,
+						u: 0, v: 0, w: 1
+					},
+					dimensions: {
+						horz: 0,
+						vert: 0
+					}
+				}],
+				mdia: [{
+					atoms: {
+						mdhd: [{
+							version: 0,
+							flags: 0,
+							timescale: 90000,
+							duration: vdata.duration,
+							lang: 'eng'
+						}],
+						hdlr: [{
+							version: 0,
+							flags: 0,
+							handler_type: 'soun',
+							name: 'SoundHandler'
+						}],
+						minf: [{
+							atoms: {
+								smhd: [{
+									version: 0,
+									flags: 0,
+									balance: 0
+								}],
+								dinf: [{
+									atoms: {
+										dref: [{
+											version: 0,
+											flags: 0,
+											entries: [{
+												type: 'url ',
+												version: 0,
+												flags: 1,
+												location: ''
+											}]
+										}]
+									}
+								}],
+								stbl: [{
+									atoms: {
+										stsd: [{
+											version: 0,
+											flags: 0,
+											entries: [{
+												type: 'mp4a',
+												data_reference_index: 1,
+												channelcount: 2,
+												samplesize: 16,
+												samplerate: 22050,
+												atoms: {
+													esds: [{
+														version: 0,
+														flags: 0,
+														sections: [
+															{
+																descriptor_type: 3,
+																ext_type: 128,
+																length: 34,
+																es_id: 2,
+																stream_priority: 0
+															},
+															{
+																descriptor_type: 4,
+																ext_type: 128,
+																length: 20,
+																type: 'mpeg4_audio',
+																stream_type: 'audio',
+																upstream_flag: 0,
+																buffer_size: 0,
+																maxBitrate: Math.round(adata.maxAudioSize / (vdata.duration / 90000 / adata.audioSizes.length)),
+																avgBitrate: Math.round((adata.data.byteLength) / (vdata.duration / 90000))
+															},
+															{
+																descriptor_type: 5,
+																ext_type: 128,
+																length: 2,
+																audio_profile: adata.profileMinusOne + 1,
+																sampling_freq: adata.samplingFreq,
+																channelConfig: adata.channelConfig
+															},
+															{
+																descriptor_type: 6,
+																ext_type: 128,
+																length: 1,
+																sl: 2
+															}
+														]
+													}]
+												}
+											}]
+										}],
+										stts: [{
+											version: 0,
+											flags: 0,
+											entries: [{
+												sample_count: adata.audioSizes.length,
+												sample_delta: Math.round(vdata.duration / adata.audioSizes.length)
+											}]
+										}],
+										stsc: [{
+											version: 0,
+											flags: 0,
+											entries: [{
+												first_chunk: 1,
+												samples_per_chunk: adata.audioSizes.length,
+												sample_description_index: 1
+											}]
+										}],
+										stsz: [{
+											version: 0,
+											flags: 0,
+											sample_size: 0,
+											sample_count: adata.audioSizes.length,
+											sample_sizes: adata.audioSizes
+										}],
+										stco: [{
+											version: 0,
+											flags: 0,
+											entries: [0x28 + audioStart]
+										}]
+									}
+								}]
+							}
+						}]
+					}
+				}]
+			}
+		});
+	}
+
+	mp4.write('File', {
+		ftyp: [{
+			major_brand: 'isom',
+			minor_version: 512,
+			compatible_brands: ['isom', 'iso2', 'avc1', 'mp41']
+		}],
+		mdat: [{
+			_rawData: mdat
+		}],
+		moov: [{
+			atoms: {
+				mvhd: [{
+					version: 0,
+					flags: 0,
+					creation_time: creationTime,
+					modification_time: creationTime,
+					timescale: 90000,
+					duration: vdata.duration,
+					rate: 1,
+					volume: 1,
+					matrix: {
+						a: 1, b: 0, x: 0,
+						c: 0, d: 1, y: 0,
+						u: 0, v: 0, w: 1
+					},
+					next_track_ID: 2
+				}],
+				trak: trak
+			}
+		}]
+	});
+	
+	return mp4.slice(0, mp4.tell()).view.getBytes();
+}
